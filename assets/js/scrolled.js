2ac3a24c04d88c3bbbf599cc2abff4cce9d1a521
yiinuma <y.iinuma211@gmail.com>
2020-12-21 20:56:07 +0900

    master

--- assets/js/scrolled.js
+++ assets/js/scrolled.js
@@ -3,6 +3,7 @@ $(function () {
         // masthead scroll
         var header = $('.l-header'); // fixed DOM
         var headerIn = $('.p-header__inner'); // fixed DOM
+        var burger = $('.c-burger__line');
         var addclass = 'scrolled'; // add css class
         var offset = header.offset();
         var scrollY = offset.top + 0; // scroll
@@ -11,9 +12,11 @@ $(function () {
             if ($(window).scrollTop() > scrollY) {
                 header.addClass(addclass);
                 headerIn.addClass(addclass);
+                burger.addClass(addclass);
             } else {
                 header.removeClass(addclass);
                 headerIn.removeClass(addclass);
+                burger.removeClass(addclass);
             }
         });
     });
